2e491e5ccef90ec5d149c7e8c4641345859c53a1
Matthew Morrison <matt.morrison@nd.edu>
2020-10-06 18:02:22 -0400

    Lab08 Update

---
+++ Lab08/Lab08.cpp
@@ -0,0 +1,20 @@
+#include "palindrome.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main ( )
+{
+
+   /* declare vectors */
+
+   /* prompt user and input sentence */
+
+   /* reverse the sentence */
+
+   /* compare the vectors and output results */
+
+
+   return 0;
+}
